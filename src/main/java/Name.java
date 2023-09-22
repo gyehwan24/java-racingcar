9ef6b97bbf02013b397b6537e98442107d68e0f2
Gyehwan Baek <b99hwan@gmail.com>
2023-09-23 03:34:17 +0900

    [REFACTOR]: name 멤버 변수에 final 추가

--- src/main/java/Name.java
+++ src/main/java/Name.java
@@ -8,7 +8,7 @@ public class Name {
     private static final int MAX_NAME_LENGTH = 5;
     private static final String NAME_SPLITTER = ",";
 
-    private String name;
+    private final String name;
 
     public Name(String name) {
         this.name = name;
